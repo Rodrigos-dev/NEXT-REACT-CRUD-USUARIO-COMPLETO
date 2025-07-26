e3a475029b00210f25724bccfd298f9b4eb1a105
Rodrigos-dev <rodrigos.ars.dev@gmail.com>
2025-07-26 12:31:13 -0300

    feat: ccs in gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  
+  basePath: '/NEXT-REACT-CRUD-USUARIO-COMPLETO',
+  // assetPrefix: '/NEXT-REACT-CRUD-USUARIO-COMPLETO/',
   reactStrictMode: true,
 };
 
