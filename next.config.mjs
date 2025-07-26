b100d57771287791c9b68194bde245a3a7f70167
Rodrigos-dev <rodrigos.ars.dev@gmail.com>
2025-07-26 19:08:17 -0300

    feat: deploy in vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const inGitHubPages = process.env && process.env.NODE_ENV === 'inGitHubPages';
-
-const nextConfig = {
-  output: inGitHubPages ? 'export' : '',  
-  basePath: inGitHubPages ? '/NEXT-REACT-CRUD-USUARIO-COMPLETO': '',
-  assetPrefix: inGitHubPages ? '/NEXT-REACT-CRUD-USUARIO-COMPLETO/' : '',
-  images: {
-      unoptimized: true,
-    },
+const nextConfig = {  
   reactStrictMode: true,
 };
 
