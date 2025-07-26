97b8ecac5d32c3d517c049e4a3f6212b19eab666
Rodrigos-dev <rodrigos.ars.dev@gmail.com>
2025-07-26 12:19:04 -0300

    feat: add gh pages ok

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/NEXT-REACT-CRUD-USUARIO-COMPLETO',
-  assetPrefix: '/NEXT-REACT-CRUD-USUARIO-COMPLETO/',
-  reactStrictMode: true,
-};
-
-export default nextConfig;
