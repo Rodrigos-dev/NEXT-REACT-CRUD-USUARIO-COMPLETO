070197b6ff2a7261a8f10fbdbd5f0d8be84c9c7c
Rodrigos-dev <rodrigos.ars.dev@gmail.com>
2025-07-26 13:10:22 -0300

    feat: gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'export',  
   basePath: '/NEXT-REACT-CRUD-USUARIO-COMPLETO',
   assetPrefix: '/NEXT-REACT-CRUD-USUARIO-COMPLETO',
+  images: {
+      unoptimized: true,
+    },
   reactStrictMode: true,
 };
 
